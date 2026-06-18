9fcc5656b3b17d1477007bd092d8531549ccf745
PhucCodeChill <kphuchuynh2005@gmail.com>
2026-06-19 00:22:47 +0700

    first commit

---
+++ lessons/bai2_1/solution.ts
@@ -0,0 +1,15 @@
+export class SmartContract {
+    private message: string;
+
+    constructor(initialMessage: string) {
+        // TODO: initialize the message with initialMessage
+    }
+
+    public updateMessage(newMsg: string): void {
+        // TODO: update the message with newMsg
+    }
+
+    public getMessage(): string {
+        // TODO: return the current message
+    }
+}
